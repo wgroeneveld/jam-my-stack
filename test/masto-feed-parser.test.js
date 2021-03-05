62d16df57ac1f891161c37abadb385a373fda363
wgroeneveld <wouter@brainbaking.com>
2021-03-05 11:38:51 +0100

    attempt to fix test at github-side

--- test/masto-feed-parser.test.js
+++ test/masto-feed-parser.test.js
@@ -7,34 +7,35 @@ const frontMatterParser = require('parser-front-matter');
 jest.mock('got');
 
 const { parseMastoFeed } = require('../src/masto-feed-parser')
+const dumpdir = `${__dirname}/dump`
 
 describe("mastodon feed parser tests", () => {
 	beforeEach(() => {
-		if(fs.existsSync("dump")) {
-			rmdir("dump")
+		if(fs.existsSync(dumpdir)) {
+			rmdir(dumpdir)
 		}
-		fs.mkdirSync("dump")
+		fs.mkdirSync(dumpdir)
 	});
 
 	test("parse creates separate notes in each month subdir", async () => {
 		await parseMastoFeed({
 			url: "invalid",
-			notesdir: "dump"
+			notesdir: dumpdir
 		})
 
-		let dir = await fsp.readdir("dump/2021/02", { withFileTypes: true })
+		let dir = await fsp.readdir(`${dumpdir}/2021/02`, { withFileTypes: true })
 		expect(dir.length).toBe(8)
-		dir = await fsp.readdir("dump/2021/03", { withFileTypes: true })
+		dir = await fsp.readdir(`${dumpdir}/2021/03`, { withFileTypes: true })
 		expect(dir.length).toBe(4)
 	})
 
 	test("parse creates correct MD structure", async () => {
 		await parseMastoFeed({
 			url: "invalid",
-			notesdir: "dump"
+			notesdir: dumpdir
 		})
 
-		const actualMd = await fsp.readFile(`${__dirname}/../dump/2021/03/01h20m3s35.md`).toString()
+		const actualMd = await fsp.readFile(`${dumpdir}/2021/03/01h20m3s35.md`).toString()
 		const expectedMd = await fsp.readFile(`${__dirname}/expected-01h20m3s35.md`).toString()
 
 		expect(actualMd).toEqual(expectedMd)
@@ -44,10 +45,10 @@ describe("mastodon feed parser tests", () => {
 		//https://aus.social/users/aussocialadmin/statuses/105817435308293091
 		await parseMastoFeed({
 			url: "invalid",
-			notesdir: "dump"
+			notesdir: dumpdir
 		})
 
-		const actualMd = await fsp.readFile(`${__dirname}/../dump/2021/03/02h17m18s46.md`)
+		const actualMd = await fsp.readFile(`${dumpdir}/2021/03/02h17m18s46.md`)
 		const expectedReplyTo = "https://social.linux.pizza/users/StampedingLonghorn/statuses/105821099684887793"
 
 		const md = frontMatterParser.parseSync(actualMd.toString())
